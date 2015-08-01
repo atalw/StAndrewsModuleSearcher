f43e4eda1fc6e1fd8b174b71e6c40b0a0598232d
Akshit Talwar <at220@st-andrews.ac.uk>
2015-08-01 20:14:54 +0530

    added module links; json file with all data

--- scraping.js
+++ scraping.js
@@ -1,5 +1,6 @@
 var request = require('request'),
 	cheerio = require('cheerio'),
+	fs = require('fs'),
 	rows = [];
 
 
@@ -9,16 +10,19 @@ request('https://portal.st-andrews.ac.uk/catalogue/Search?dept=xxALL&level=ALL',
 		$('tbody tr', '#content').each(function(i, tr) {
 			var child = $(this).children();
 			var moduleCode = child.eq(0);
+			var moduleLink = child.eq(0).children().eq(0);
 			var year = child.eq(1);
 			var title = child.eq(2);
 			var row = {
 				"moduleCode": moduleCode.text(),
+				"moduleLink": moduleLink.attr('href'),
 				"year": year.text(),
 				"title": title.text(),
 			};
 			rows.push(row);
-			console.log(row);
 		});
-		console.log(rows);
+		fs.writeFile('data.json', JSON.stringify(rows, null, 4), function(err) {
+			console.log("successful");
+		})
 	}
 });
