963fe34c29ab292320d7bd98b149bc5c51403592
Akshit Talwar <at220@st-andrews.ac.uk>
2015-08-02 22:13:41 +0530

    extended scraping for more details

--- scraping.js
+++ scraping.js
@@ -4,30 +4,44 @@ var request = require('request'),
 	allModules = [],
 	rows = [];
 
-
+var moduleLink;
 request('https://portal.st-andrews.ac.uk/catalogue/Search?dept=xxALL&level=ALL', function(err, resp, body) {
-	if( !err && resp.statusCode == 200) {
-		var $ = cheerio.load(body);
-		$('tbody tr', '#content').each(function(i, tr) {
-			var child = $(this).children();
-			var moduleCode = child.eq(0);
-			var moduleLink = child.eq(0).children().eq(0);
-			var year = child.eq(1);
-			var title = child.eq(2);
-			var row = {
-				"moduleCode": moduleCode.text(),
-				"moduleLink": moduleLink.attr('href'),
-				"year": year.text(),
-				"title": title.text(),
-			};
-			rows.push(row);
-			allModules.push(moduleCode.text());
-		});
-		fs.writeFile('data.json', JSON.stringify(rows, null, 4), function(err) {
-			console.log("successful writing rows");
-		});
-		fs.writeFile('moduleCodes.json', JSON.stringify(allModules, null, 4), function(err) {
-			console.log("successful writing module codes");
-		});
-	}
+		if( !err && resp.statusCode == 200) {
+			var $ = cheerio.load(body);
+			$('tbody tr', '#content').each(function(i, tr) {
+				var child = $(this).children();
+				var moduleCode = child.eq(0);
+				moduleLink = child.eq(0).children().eq(0);
+				var year = child.eq(1);
+				var title = child.eq(2);
+				var row = {
+						"moduleCode": moduleCode.text(),
+						"moduleLink": moduleLink.attr('href'),
+						"year": year.text(),
+						"title": title.text()
+				};
+				allModules.push(moduleCode.text());
+				request('https://portal.st-andrews.ac.uk/catalogue/' + moduleLink.attr('href'), function(err, resp, body) {
+						if ( !err && resp.statusCode == 200) {
+							var $ = cheerio.load(body);
+							$('tr', '#content').each(function(i, tr) {
+								var child = $(this).children();
+								var deet = child.eq(0);
+								var val = child.eq(1);
+								var deetText = deet.text();
+								row[deetText] = val.text();
+							});
+							console.log(row);
+							rows.push(row);
+						}
+						fs.writeFile('data1.json', JSON.stringify(rows, null, 4), function(err) {
+							console.log("successful writing rows");
+						});
+				});
+			});
+			fs.writeFile('moduleCodes.json', JSON.stringify(allModules, null, 4), function(err) {
+				console.log("successful writing module codes");
+			});
+		}
 });
+
